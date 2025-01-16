9f2974bba3f647af714db185689588da804abd90
Marcus Gabriel <marcusgx45@gmail.com>
2025-01-16 18:33:55 -0300

    Atualiza links de estilo e chamadas AJAX para usar SCRIPT_URI em vez de URLs absolutas

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://<?php echo $_SERVER['SCRIPT_URI']; ?>styles/config.css">
+    <link rel="stylesheet" href="<?php echo $_SERVER['SCRIPT_URI']; ?>styles/config.css">
     <title>Document</title>
 </head>
 
@@ -34,7 +34,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
         $.ajax({
-            url: 'https://<?php echo $_SERVER['SCRIPT_URI']; ?>public/header',
+            url: '<?php echo $_SERVER['SCRIPT_URI']; ?>public/header',
             type: 'POST',
             data: {
                 img: 'one',
