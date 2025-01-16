1c072a98ec66b241fda163ab11a8f026677e5729
Marcus Gabriel <marcusgx45@gmail.com>
2025-01-16 14:37:39 -0300

    Substitui o conteúdo de index.php por uma estrutura HTML básica com uma mensagem de teste

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-
-echo "ola mundo";
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    este é um teste
+</body>
+</html>
