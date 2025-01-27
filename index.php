4b4c4cc84ab519543a9372f82be2713c96b03006
Marcus Gabriel <marcusgx45@gmail.com>
2025-01-27 10:20:21 -0300

    Atualiza a referência do CSS em index.php para usar uma variável de URL dinâmica

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ($_SERVER['SSL_TLS_SNI'] == 'teste.sicoobcredimata.com.br') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://<?php echo $_SERVER['SSL_TLS_SNI']; ?>/styles/config.css">
+    <link rel="stylesheet" href="<?php echo $url; ?>/styles/config.css">
     <title>Document</title>
 </head>
 
