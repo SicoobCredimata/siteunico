5f27fd38c70ac5ef3a809e93a332bc0f1f26bbb6
Marcus Gabriel <marcusgx45@gmail.com>
2025-01-28 14:57:39 -0300

    Ajusta caminhos de arquivos para uso relativo no módulo surveyNPS

--- modules/surveyNPS.php
+++ modules/surveyNPS.php
@@ -48,7 +48,7 @@ if (isset($_GET['pa'])) {
     <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../libs/toast/toast.css">
-    <link rel="shortcut icon" href="/imgs/system/icons/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="../imgs/system/icons/favicon.svg" type="image/x-icon">
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
@@ -63,7 +63,7 @@ if (isset($_GET['pa'])) {
             align-items: center;
             margin: 0;
             padding: 10px;
-            background-image: url('/../libs/background/fundo_claro.jpg');
+            background-image: url('../libs/background/fundo_claro.jpg');
             background-size: cover;
             background-repeat: no-repeat;
             min-height: 100vh;
